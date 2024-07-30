02c9a781f64975c13b6ac473efac3e6945127b03
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-30 20:23:29 +0330

    Create Contact Us Model And Create App Db Context

---
+++ Contact-US/Models/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Contact_US.Models;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<contactUsEntity> ContactUS { get; set; }
+    
+}
---
+++ Contact-US/Models/contactUsEntity.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Contact_US.Models;
+
+
+[Table(name:"ContactUS")]
+public class contactUsEntity
+{
+    
+    [Key] 
+    [Required]
+    [SwaggerIgnore]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Please Enter Name")]
+    [Display(Name = "Name")]
+    [MaxLength(350)]
+    public string Name { get; set; }
+    
+    
+    [Required(ErrorMessage = "Please Enter Email")]
+    [Display(Name = "Email")]
+    [MaxLength(350)]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Please Enter Number")]
+    [Display(Name = "Number")]
+    [MaxLength(350)]
+    public string Number { get; set; }
+    
+    [Required(ErrorMessage = "Please Enter Your Text")]
+    [Display(Name = "Text")]
+    [MaxLength(350)]
+    public string Text { get; set; }
+}
