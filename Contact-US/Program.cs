04383e877f0d4e74288c5939fabe1eb43b258423
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-30 20:30:09 +0330

    Connect To Sql server

--- Contact-US/Program.cs
+++ Contact-US/Program.cs
@@ -1,9 +1,20 @@
+using Contact_US.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString"));
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
