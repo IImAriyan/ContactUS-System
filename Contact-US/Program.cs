fed876268d52793469233fcdd3f170aa52b5d299
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-30 22:23:01 +0330

    Auto Migrate

--- Contact-US/Program.cs
+++ Contact-US/Program.cs
@@ -17,6 +17,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
