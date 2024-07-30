df70f77de32bb0b44ec00bb4e82ef2f2256fd763
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-30 20:46:39 +0330

    Add Migrations And Update Database

--- Contact-US/Controllers/ContactUSController.cs
+++ Contact-US/Controllers/ContactUSController.cs
@@ -1,11 +1,19 @@
+using Contact_US.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Contact_US.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
-    public class ContactUSController : ControllerBase
+    public class ContactUSController(AppDbContext dbContext) : ControllerBase
     {
+        // Get Messages 
+        [HttpGet("ContactUS/Messages")]
+        public IEnumerable<contactUsEntity> getMessages()
+        {
+            return dbContext.ContactUS;
+        }
+        
     }
 }
---
+++ Contact-US/Migrations/20240730171434_arian.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Contact_US.Migrations
+{
+    /// <inheritdoc />
+    public partial class arian : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactUS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(350)", maxLength: 350, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(350)", maxLength: 350, nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(350)", maxLength: 350, nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(350)", maxLength: 350, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactUS", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactUS");
+        }
+    }
+}
