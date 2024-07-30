d4df524a5af9f9107515e8dba093f767d67c0928
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-30 20:53:24 +0330

    Add Adding Message Function

--- Contact-US/Controllers/ContactUSController.cs
+++ Contact-US/Controllers/ContactUSController.cs
@@ -1,6 +1,7 @@
 using Contact_US.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Contact_US.Controllers
 {
@@ -13,6 +14,23 @@ namespace Contact_US.Controllers
         public IEnumerable<contactUsEntity> getMessages()
         {
             return dbContext.ContactUS;
+        }
+        
+        //Add Messages
+        [HttpPost("ContactUS/Add")]
+        public async Task<ActionResult<contactUsEntity>> addMessage(contactUsEntity dto)
+        {
+            EntityEntry<contactUsEntity> Message = dbContext.ContactUS.Add(dto);
+
+            await dbContext.SaveChangesAsync();
+
+            Response.StatusCode = 200;
+            var messageResponse = new
+            {
+                Sataus = 200,
+                Message = "Message Successfully Send"
+            };
+            return Ok(messageResponse);
         }
         
     }
